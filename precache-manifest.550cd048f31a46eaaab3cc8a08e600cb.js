3bab9a68773588538771a576f96fc857248c3f5d
Adam Hałdaś <adam.haldas@gmail.com>
2020-09-27 14:20:34 +0200

    Updates

---
+++ precache-manifest.550cd048f31a46eaaab3cc8a08e600cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68521c560554e845db1dad915346b09",
+    "url": "/WitcherForm/index.html"
+  },
+  {
+    "revision": "ec86441cb67618e3ff7e",
+    "url": "/WitcherForm/static/css/main.32a7809f.chunk.css"
+  },
+  {
+    "revision": "5e626a62d8551ee056ef",
+    "url": "/WitcherForm/static/js/2.63679bb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WitcherForm/static/js/2.63679bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec86441cb67618e3ff7e",
+    "url": "/WitcherForm/static/js/main.e7c6052f.chunk.js"
+  },
+  {
+    "revision": "4159d53951ff5edbca2f",
+    "url": "/WitcherForm/static/js/runtime-main.95b51014.js"
+  },
+  {
+    "revision": "f35e9c840106bcefd9c3b158e73d4324",
+    "url": "/WitcherForm/static/media/logo.f35e9c84.png"
+  },
+  {
+    "revision": "da6f80d41a6af0b229f0476f4a934d63",
+    "url": "/WitcherForm/static/media/witcher4.da6f80d4.png"
+  }
+]);
